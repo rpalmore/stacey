53811970493bbe728de31bbfc938978b5ef59b87
rebecca palmore <rkpalmore@gmail.com>
2018-06-26 14:17:33 -0500

    testing colors and layout changes

--- public/index.js
+++ public/index.js
@@ -17,7 +17,7 @@ toggleVid = () => {
 
 TweenLite.to('#welcome', 1, {autoAlpha:1, delay:.2});
 // TweenLite.to('.header', 1, {autoAlpha:0.5, delay:3});
-TweenLite.to('.image', 1, {autoAlpha:1, delay:1});
+// TweenLite.to('.image', 1, {autoAlpha:1, delay:1});
 // TweenLite.to('#welcome', .5, {autoAlpha: 0, delay: 3});
 // TweenLite.to('#welcome', .5, {autoAlpha: 1, width: 'auto', top: 0, delay:3.5});
 // TweenLite.to('.menuBoxes', 1, {autoAlpha:1, delay:3});
