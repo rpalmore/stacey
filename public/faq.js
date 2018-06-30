feb27c143a4c381e720895cd50f6e7099bc73288
rebecca palmore <rkpalmore@gmail.com>
2018-06-29 23:37:46 -0500

    refactored javascript for Greensock ScrollTo

--- public/faq.js
+++ public/faq.js
@@ -13,6 +13,7 @@ toggleVid = () => {
 TweenLite.to('#welcome', 1, {autoAlpha:1, delay:.2});
 // TweenLite.to('.image', 1, {autoAlpha:1, delay:7});
 
+// Materialize sidenav
 document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.sidenav');
   var instances = M.Sidenav.init(elems, options);
@@ -23,62 +24,23 @@ document.addEventListener('DOMContentLoaded', function() {
   };
 });
 
-var question = "";
-var answer = "";
-
-for (var _n = 1; _n < 10; _n++) {
-  var question_n = document.getElementById("q"+_n);
-  var answer_n = document.getElementById("answer"+_n);
-
-};
-
+// FAQ Greensock ScrollTo
 var btn = document.getElementsByClassName("btn-floating");
+var q = document.getElementsByClassName("questions");
+var answer = document.getElementsByClassName("qRepeat");
 
-for (var i = 0; i < btn.length; i++) {
-  btn[i].onclick = function() {
+
+for (var i = 0; i < q[0].children.length; i++) {
+  (function(index) {
+    q[0].children[i].onclick = function() {
+      let m = (index);
+      TweenLite.to(window, .7, {scrollTo:{y:answer[m], offsetY:5}});
+    }   
+  })(i);
+    btn[i].onclick = function() {
     TweenLite.to(window, .7, {scrollTo:{y:"nav"}})
-  }
+  };
 };
-
-// TO DO: CLEAN ALL OF THIS UP SO NO REPETITION
-
-q1.onclick = function() {
-  TweenLite.to(window, .7, {scrollTo:{y:"#answer1", offsetY:5}});
-};
-
-q2.onclick = function() {
-  TweenLite.to(window, .7, {scrollTo:{y:"#answer2", offsetY:5}});
-};
-
-q3.onclick = function() {
-  TweenLite.to(window, .7, {scrollTo:{y:"#answer3", offsetY:5}});
-};
-
-q4.onclick = function() {
-  TweenLite.to(window, .7, {scrollTo:{y:"#answer4", offsetY:5}});
-};
-
-q5.onclick = function() {
-  TweenLite.to(window, .7, {scrollTo:{y:"#answer5", offsetY:5}});
-};
-
-q6.onclick = function() {
-  TweenLite.to(window, .7, {scrollTo:{y:"#answer6", offsetY:5}});
-};
-
-q7.onclick = function() {
-  TweenLite.to(window, .7, {scrollTo:{y:"#answer7", offsetY:5}});
-};
-
-q8.onclick = function() {
-  TweenLite.to(window, .7, {scrollTo:{y:"#answer8", offsetY:5}});
-};
-
-q9.onclick = function() {
-  TweenLite.to(window, .7, {scrollTo:{y:"#answer9", offsetY:5}});
-};
-
-
 
 
 
